0ee3ae6bcf9c018757e6e93a0f0d7e5a284ac04f
Joshua Seelye <josh@Joshuas-MacBook-Pro.local>
2017-11-14 21:39:57 -0600

    no message

---
+++ StatModifiers.hpp
@@ -0,0 +1,19 @@
+//
+//  StatModifiers.hpp
+//  GameFirstDraft
+//
+//  Created by Joshua Seelye on 11/13/17.
+//  Copyright © 2017 Josh Seelye. All rights reserved.
+//
+
+#ifndef StatModifiers_h
+#define StatModifiers_h
+
+#include "Item.hpp"
+
+class StatModifiers : public Item
+{
+    
+};
+
+#endif /* StatModifiers_h */
