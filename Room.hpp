a24ff058431d4796a219ff82997b4948716eef4c
Kage Kowalski <kagekowalski@gmail.com>
2017-11-15 23:49:30 -0600

    Fixed references & addresses in Room.hpp and Stage.hpp

--- Room.hpp
+++ Room.hpp
@@ -1,12 +1,13 @@
 #include <vector>
+#include <assert.h>
 #include "Monster.hpp"
 #include "Interactable.hpp"
 
 class Room{
 	public:
 		//  Initializes all of this Room's data.
-		Room(int _id, int _north, int _east, int _south, int _west, vector<Monster*> _monsters, vector<Interactable*> _interactables):
-			id(_id), north(_north), east(_east), south(_south), west(_west), monsters(_monsters), interactables(_interactables) {}
+		Room(int _id, int _north, int _east, int _south, int _west):
+			id(_id), north(_north), east(_east), south(_south), west(_west) {}
 
 
 		//  Return true if this Room has designated exit, else false.
@@ -29,14 +30,20 @@ class Room{
 
 
 		//  Return vector of pointers to Monsters or Interactables in this Room.
-		vector<Monster*> get_monsters() {return monsters;}
-		vector<Interactable*> get_interactables() {return interactables;}
+		vector<Monster>& get_monsters() {return monsters;}
+		vector<Interactable>& get_interactables() {return interactables;}
+
+
+		//  Adds passed Monster or Interactable to this Room.
+		//  Retursn true if successful, else false.
+		void add_monster(Monster&);
+		void add_interactable(Interactable&);
 
 
 		//  Removes passed Monster or Interactable from this Room.
 		//  Returns true if successful, else false.
-		bool remove_monster(Monster*);
-		bool remove_interactable(Interactable*);
+		void remove_monster(Monster&);
+		void remove_interactable(Interactable&);
 
 
 	protected:
@@ -45,33 +52,43 @@ class Room{
 		int east;
 		int south;
 		int west;
-		vector<Monster*> monsters;
-		vector<Interactable*> interactables;
+		vector<Monster> monsters;
+		vector<Interactable> interactables;
 };
 
 
 //  MUTATORS
 
 
-bool Room::remove_monster(Monster* mo){
-	vector<Monster*> updatedMonsters;
-	bool monsterFound = false;
-	for(int i = 0; i < monsters.size(); i++){
-		if(monsters.at(i) != mo) updatedMonsters.push_back(monsters.at(i));
-		else monsterFound = true;
-	}
-	monsters = updatedMonsters;
-	return monsterFound;
+void Room::add_monster(Monster& mo){
+	monsters.push_back(mo);
 }
 
 
-bool Room::remove_interactable(Interactable* in){
-	vector<Interactable*> updatedInteractables;
+void Room::add_interactable(Interactable& in){
+	interactables.push_back(in);
+}
+
+
+void Room::remove_monster(Monster& mo){
+	vector<Monster> updatedMonsters;
+	bool monsterFound = false;
+	for(int i = 0; i < monsters.size(); i++){
+		if(&monsters.at(i) != &mo) updatedMonsters.push_back(monsters.at(i));
+		else monsterFound = true;
+	}
+	monsters = updatedMonsters;
+	assert(monsterFound);
+}
+
+
+void Room::remove_interactable(Interactable& in){
+	vector<Interactable> updatedInteractables;
 	bool interactableFound = false;
 	for(int i = 0; i < interactables.size(); i++){
-		if(interactables.at(i) != in) updatedInteractables.push_back(interactables.at(i));
+		if(&interactables.at(i) != &in) updatedInteractables.push_back(interactables.at(i));
 		else interactableFound = true;
 	}
 	interactables = updatedInteractables;
-	return interactableFound;
+	assert(interactableFound);
 }
