b935f0a6ae8d033922f19a49445202eed720d40b
Klayton Kowalski <klaytonkowalski@gmail.com>
2018-02-22 16:39:55 -0600

    Added comments, added file ownership comments

--- Combatant.cpp
+++
@@ -1,4 +0,0 @@
-#include "Combatant.h"
-
-Combatant::Combatant(int vit, int str, int def, int lck, int spd, int tme, int gld, int exp) : _vit(vit), _str(str), _def(def), _lck(lck), _spd(spd), _tme(tme), _gld(gld), _exp(exp), Entity("Generic Combatant")
-{}
