18ba94f3d9e909be14e665f87bceda01725db503
Klayton Kowalski <klaytonkowalski@gmail.com>
2018-02-22 20:34:56 -0600

    Tileset class

--- TileSet.cpp
+++ TileSet.cpp
@@ -1,12 +1,32 @@
+// Klayton Kowalski
+
 #include "TileSet.h"
 
-
-
-TileSet::TileSet()
-{
+TileSet::TileSet() {
+	m_Width = 0;
+	m_Height = 0;
 }
 
+TileSet::~TileSet() {}
 
-TileSet::~TileSet()
-{
+bool TileSet::load(std::string tilesetFileName) {
+	std::string filePath = tilesetFileName;
+
+	if (!m_TileSet.loadFromFile(filePath)) return false;
+	m_Width = m_TileSet.getSize().x;
+	m_Height = m_TileSet.getSize().y;
+
+	return true;
+}
+
+const sf::Texture& TileSet::getTexture() const {
+	return m_TileSet;
+}
+
+unsigned int TileSet::getWidth() const {
+	return m_Width;
+}
+
+unsigned int TileSet::getHeight() const {
+	return m_Height;
 }
--- TileSet.h
+++ TileSet.h
@@ -5,8 +5,6 @@
 
 #include <SFML/Graphics.hpp>
 
-
-
 class TileSet {
 	
 public:
@@ -14,9 +12,20 @@ public:
 	TileSet();
 	~TileSet();
 
+	bool load(std::string tilesetFileName);
+
+	const sf::Texture& getTexture() const;
+	unsigned int getWidth() const;
+	unsigned int getHeight() const;
+
 private:
 
 	sf::Texture m_TileSet;
+
+	// Width of the tileset in tile units
+	unsigned int m_Width;
+	// Height of the tileset in tile units
+	unsigned int m_Height;
 
 };
 
