4d9cbd1c3128874824b585085e8b5de9243fef40
Joshua Seelye <jseelye@mail.bradley.edu>
2018-03-02 17:34:21 -0600

    added two layers, Fixed issue with blank maps, fully commented tilemap.h

--- TileMap.cpp
+++ TileMap.cpp
@@ -12,14 +12,23 @@ TileMap::Tile::Tile(int ID, const sf::Vector2f& postition) : _tileID(ID), _step(
 //Moves the animation step forward one if the the time exceeds that last 
 void TileMap::Tile::updateAnimStep(sf::Time deltaTime)
 {
-    //arbitrary value until i consult Klayton
-    _animate.update(deltaTime);
-    _step = _animate.getCurrAnimStep();
+    if(_animate.getTotalAnimSteps() > 1)
+    {
+        _animate.update(deltaTime);
+        _step = _animate.getCurrAnimStep();
+    }
 }
-
-int  TileMap::Tile::getID()         { return _tileID; }
-void TileMap::Tile::setID(int ID)   { _tileID = ID;   }
-int  TileMap::Tile::getAnimStep()   { return _step;   }
+void TileMap::Tile::setAnimStep(unsigned int step)
+{
+    if(step <= _animate.getTotalAnimSteps())
+    {
+        _animate.setCurrAnimStep(step);
+        _step = step;
+    }
+}
+int  TileMap::Tile::getID()        { return _tileID; }
+void TileMap::Tile::setID(int ID)  { _tileID = ID;   }
+int  TileMap::Tile::getAnimStep()  { return _step;   }
 const sf::Vector2f& TileMap::Tile::getPosition() const
 {
     return _position;
@@ -32,37 +41,47 @@ const sf::Vector2f& TileMap::Tile::getPosition() const
 TileMap::TileMap(const sf::Vector2f& scale, std::string music) : _music(music)
 {
 	_groundVerticies.setPrimitiveType(sf::PrimitiveType::Quads);
-    _layerOneVerticies.setPrimitiveType(sf::PrimitiveType::Quads);
     _layerTwoVerticies.setPrimitiveType(sf::PrimitiveType::Quads);
+    _layerThreeVerticies.setPrimitiveType(sf::PrimitiveType::Quads);
+    _layerSixVerticies.setPrimitiveType(sf::PrimitiveType::Quads);
+    _canopyVerticies.setPrimitiveType(sf::PrimitiveType::Quads);
 	setScale(scale);
 }
 
-bool TileMap::build(int* ground, int* layerOne, int* layerTwo, int width, int height, std::string tilesetFileName)
+bool TileMap::build(int* ground, int* layerTwo, int* layerThree, int* layerSix, int* canopy, int width, int height, std::string tilesetFileName)
 {
     if(!_tileset.load(tilesetFileName)) return false;
     _width = width;
     _height = height;
 
-	_groundVerticies.resize(_width * _height * 4 * 3);
-    _layerOneVerticies.resize(_width * _height * 4 * 3);
-    _layerTwoVerticies.resize(_width * _height * 4 * 3);
+	_groundVerticies.resize(_width * _height * 4    );
+    _layerTwoVerticies.resize(_width * _height * 4  );
+    _layerThreeVerticies.resize(_width * _height * 4);
+    _layerSixVerticies.resize(_width * _height * 4  );
+    _canopyVerticies.resize(_width * _height * 4    );
 
-    _ground   = new Tile*[_height];
-    _layerOne = new Tile*[_height];
-    _layerTwo = new Tile*[_height];
+    _ground     = new Tile*[_height];
+    _layerTwo   = new Tile*[_height];
+    _layerThree = new Tile*[_height];
+    _layerSix   = new Tile*[_height];
+    _canopy     = new Tile*[_height];
     for(int i = 0; i<_height;i++)
     {
-        _ground[i]   = new Tile[_width];
-        _layerOne[i] = new Tile[_width];
-        _layerTwo[i] = new Tile[_width];
+        _ground[i]     = new Tile[_width];
+        _layerTwo[i]   = new Tile[_width];
+        _layerThree[i] = new Tile[_width];
+        _layerSix[i]   = new Tile[_width];
+        _canopy[i]     = new Tile[_width];
     }
     for(int i = 0; i < _height; i++)
     {
         for(int j = 0; j < _width; j++)
         {
-            _ground[i][j]   = Tile(ground[i*_width+j],   sf::Vector2f(static_cast<float>(i), static_cast<float>(j)));
-            _layerOne[i][j] = Tile(layerOne[i*_width+j], sf::Vector2f(static_cast<float>(i), static_cast<float>(j)));
-            _layerTwo[i][j] = Tile(layerTwo[i*_width+j], sf::Vector2f(static_cast<float>(i), static_cast<float>(j)));
+            if(ground[0]!=-2) _ground[i][j]   = Tile(ground[i*_width+j],   sf::Vector2f(static_cast<float>(i), static_cast<float>(j)));
+            if(layerTwo[0]!=-2) _layerTwo[i][j] = Tile(layerTwo[i*_width+j], sf::Vector2f(static_cast<float>(i), static_cast<float>(j)));
+            if(layerThree[0]!=-2)_layerThree[i][j] = Tile(layerThree[i*_width+j], sf::Vector2f(static_cast<float>(i), static_cast<float>(j)));
+            if(layerSix[0]!=-2) _layerSix[i][j] = Tile(layerSix[i*_width+j], sf::Vector2f(static_cast<float>(i), static_cast<float>(j)));
+            if(canopy[0]!=-2) _canopy[i][j] = Tile(canopy[i*_width+j], sf::Vector2f(static_cast<float>(i), static_cast<float>(j)));
 			vertexFill(i, j);
         }
     }
@@ -74,12 +93,16 @@ TileMap::~TileMap()
     for(int i = 0; i < _height; i++)
     {
         delete[] _ground[i];
-        delete[] _layerOne[i];
         delete[] _layerTwo[i];
+        delete[] _layerThree[i];
+        delete[] _layerSix[i];
+        delete[] _canopy[i];
     }
     delete[] _ground;
-    delete[] _layerOne;
     delete[] _layerTwo;
+    delete[] _layerThree;
+    delete[] _layerSix;
+    delete[] _canopy;
 }
 
 std::string TileMap::getMusic()
@@ -91,20 +114,24 @@ void TileMap::vertexFill(int y, int x)
 {
 	// Sets up random tile, Cache ID, y position, and x position
 	int groundTileID = _ground[y][x].getID();
-    int layerOneTileID = _layerOne[y][x].getID();
     int layerTwoTileID = _layerTwo[y][x].getID();
+    int layerThreeTileID = _layerThree[y][x].getID();
+    int layerSixTileID = _layerSix[y][x].getID();
+    int canopyTileID = _canopy[y][x].getID();
 	float i = _ground[y][x].getPosition().x;
 	float j = _ground[y][x].getPosition().y;
 
 	// Calculate position of tile graphic in tileset in tile units
 	int groundTilePosX = groundTileID % static_cast<int>(_tileset.getWidth() / 16);
     int groundTilePosY = (groundTileID / _tileset.getWidth()) * 16;
-	_groundVerticies[(size_t)((i * _width + j) * 4 * 3)];
+	_groundVerticies[(size_t)((i * _width + j) * 4 )];
 
 	// Get a pointer to tile's top-left corner in vertex array
 	sf::Vertex* quadGro = &_groundVerticies[(size_t)  ((i * _width + j) * 4 )];
-    sf::Vertex* quadOne = &_layerOneVerticies[(size_t)((i * _width + j) * 4 )];
     sf::Vertex* quadTwo = &_layerTwoVerticies[(size_t)((i * _width + j) * 4 )];
+    sf::Vertex* quadThree = &_layerThreeVerticies[(size_t)((i * _width + j) * 4 )];
+    sf::Vertex* quadSix = &_layerSixVerticies[(size_t)((i * _width + j) * 4 )];
+    sf::Vertex* quadCanopy = &_canopyVerticies[(size_t)((i * _width + j) * 4 )];
 
     if(groundTileID != -1)
     {
@@ -128,34 +155,11 @@ void TileMap::vertexFill(int y, int x)
         quadGro->texCoords = sf::Vector2f(groundTilePosX * 16.0f, (groundTilePosY + 1 + _ground[y][x].getAnimStep()) * 16.0f);
         
     }
-    if(layerOneTileID != -1)
-    {
-        int layOneTilePosX = layerOneTileID % static_cast<int>(_tileset.getWidth() / 16);
-        int layOneTilePosY = (layerOneTileID / _tileset.getWidth()) * 16;
-
-        quadOne->position = sf::Vector2f(j * 16.0f, i * 16.0f);
-        quadOne->texCoords = sf::Vector2f(layOneTilePosX * 16.0f, (layOneTilePosY + _layerOne[y][x].getAnimStep()) * 16.0f);
-        
-        // Define top-right corner
-        quadOne++;
-        quadOne->position = sf::Vector2f((j + 1) * 16.0f, i * 16.0f);
-        quadOne->texCoords = sf::Vector2f((layOneTilePosX + 1) * 16.0f, (layOneTilePosY + _layerOne[y][x].getAnimStep())* 16.0f);
-        
-        // Define bottom-right corner
-        quadOne++;
-        quadOne->position = sf::Vector2f((j + 1) * 16.0f, (i + 1) * 16.0f);
-        quadOne->texCoords = sf::Vector2f((layOneTilePosX + 1) * 16.0f, (layOneTilePosY + 1 + _layerOne[y][x].getAnimStep()) * 16.0f);
-        
-        // Define bottom-left corner
-        quadOne++;
-        quadOne->position = sf::Vector2f(j * 16.0f, (i + 1) * 16.0f);
-        quadOne->texCoords = sf::Vector2f(layOneTilePosX * 16.0f, (layOneTilePosY + 1 + _layerOne[y][x].getAnimStep()) * 16.0f);
-    }
     if(layerTwoTileID != -1)
     {
         int layTwoTilePosX = layerTwoTileID % static_cast<int>(_tileset.getWidth() / 16);
         int layTwoTilePosY = (layerTwoTileID / _tileset.getWidth()) * 16;
-        
+
         quadTwo->position = sf::Vector2f(j * 16.0f, i * 16.0f);
         quadTwo->texCoords = sf::Vector2f(layTwoTilePosX * 16.0f, (layTwoTilePosY + _layerTwo[y][x].getAnimStep()) * 16.0f);
         
@@ -174,6 +178,75 @@ void TileMap::vertexFill(int y, int x)
         quadTwo->position = sf::Vector2f(j * 16.0f, (i + 1) * 16.0f);
         quadTwo->texCoords = sf::Vector2f(layTwoTilePosX * 16.0f, (layTwoTilePosY + 1 + _layerTwo[y][x].getAnimStep()) * 16.0f);
     }
+    if(layerThreeTileID != -1)
+    {
+        int layThreeTilePosX = layerThreeTileID % static_cast<int>(_tileset.getWidth() / 16);
+        int layThreeTilePosY = (layerThreeTileID / _tileset.getWidth()) * 16;
+        
+        quadThree->position = sf::Vector2f(j * 16.0f, i * 16.0f);
+        quadThree->texCoords = sf::Vector2f(layThreeTilePosX * 16.0f, (layThreeTilePosY + _layerThree[y][x].getAnimStep()) * 16.0f);
+        
+        // Define top-right corner
+        quadThree++;
+        quadThree->position = sf::Vector2f((j + 1) * 16.0f, i * 16.0f);
+        quadThree->texCoords = sf::Vector2f((layThreeTilePosX + 1) * 16.0f, (layThreeTilePosY + _layerThree[y][x].getAnimStep())* 16.0f);
+        
+        // Define bottom-right corner
+        quadThree++;
+        quadThree->position = sf::Vector2f((j + 1) * 16.0f, (i + 1) * 16.0f);
+        quadThree->texCoords = sf::Vector2f((layThreeTilePosX + 1) * 16.0f, (layThreeTilePosY + 1 + _layerThree[y][x].getAnimStep()) * 16.0f);
+        
+        // Define bottom-left corner
+        quadThree++;
+        quadThree->position = sf::Vector2f(j * 16.0f, (i + 1) * 16.0f);
+        quadThree->texCoords = sf::Vector2f(layThreeTilePosX * 16.0f, (layThreeTilePosY + 1 + _layerThree[y][x].getAnimStep()) * 16.0f);
+    }
+    if(layerSixTileID != -1)
+    {
+        int laySixTilePosX = layerSixTileID % static_cast<int>(_tileset.getWidth() / 16);
+        int laySixTilePosY = (layerSixTileID / _tileset.getWidth()) * 16;
+        
+        quadSix->position = sf::Vector2f(j * 16.0f, i * 16.0f);
+        quadSix->texCoords = sf::Vector2f(laySixTilePosX * 16.0f, (laySixTilePosY + _layerSix[y][x].getAnimStep()) * 16.0f);
+        
+        // Define top-right corner
+        quadSix++;
+        quadSix->position = sf::Vector2f((j + 1) * 16.0f, i * 16.0f);
+        quadSix->texCoords = sf::Vector2f((laySixTilePosX + 1) * 16.0f, (laySixTilePosY + _layerSix[y][x].getAnimStep())* 16.0f);
+        
+        // Define bottom-right corner
+        quadSix++;
+        quadSix->position = sf::Vector2f((j + 1) * 16.0f, (i + 1) * 16.0f);
+        quadSix->texCoords = sf::Vector2f((laySixTilePosX + 1) * 16.0f, (laySixTilePosY + 1 + _layerSix[y][x].getAnimStep()) * 16.0f);
+        
+        // Define bottom-left corner
+        quadSix++;
+        quadSix->position = sf::Vector2f(j * 16.0f, (i + 1) * 16.0f);
+        quadSix->texCoords = sf::Vector2f(laySixTilePosX * 16.0f, (laySixTilePosY + 1 + _layerSix[y][x].getAnimStep()) * 16.0f);
+    }
+    if(canopyTileID != -1)
+    {
+        int canopyTilePosX = canopyTileID % static_cast<int>(_tileset.getWidth() / 16);
+        int canopyTilePosY = (canopyTileID / _tileset.getWidth()) * 16;
+        
+        quadCanopy->position = sf::Vector2f(j * 16.0f, i * 16.0f);
+        quadCanopy->texCoords = sf::Vector2f(canopyTilePosX * 16.0f, (canopyTilePosY + _canopy[y][x].getAnimStep()) * 16.0f);
+        
+        // Define top-right corner
+        quadCanopy++;
+        quadCanopy->position = sf::Vector2f((j + 1) * 16.0f, i * 16.0f);
+        quadCanopy->texCoords = sf::Vector2f((canopyTilePosX + 1) * 16.0f, (canopyTilePosY + _canopy[y][x].getAnimStep())* 16.0f);
+        
+        // Define bottom-right corner
+        quadCanopy++;
+        quadCanopy->position = sf::Vector2f((j + 1) * 16.0f, (i + 1) * 16.0f);
+        quadCanopy->texCoords = sf::Vector2f((canopyTilePosX + 1) * 16.0f, (canopyTilePosY + 1 + _canopy[y][x].getAnimStep()) * 16.0f);
+        
+        // Define bottom-left corner
+        quadCanopy++;
+        quadCanopy->position = sf::Vector2f(j * 16.0f, (i + 1) * 16.0f);
+        quadCanopy->texCoords = sf::Vector2f(canopyTilePosX * 16.0f, (canopyTilePosY + 1 + _canopy[y][x].getAnimStep()) * 16.0f);
+    }
 }
 
 void TileMap::updateMap(sf::Time deltaTime)
@@ -183,7 +256,7 @@ void TileMap::updateMap(sf::Time deltaTime)
         for(int j = 0; j < _width; j++)
         {
             _ground[i][j].updateAnimStep(deltaTime);
-            _layerOne[i][j].updateAnimStep(deltaTime);
+            _layerTwo[i][j].updateAnimStep(deltaTime);
             _layerTwo[i][j].updateAnimStep(deltaTime);
             vertexFill(i, j);
         }
@@ -194,14 +267,23 @@ const sf::VertexArray& TileMap::getGroundVertices()   const
 {
     return _groundVerticies;
 }
-const sf::VertexArray& TileMap::getLayerOneVertices() const
-{
-    return _layerOneVerticies;
-}
 const sf::VertexArray& TileMap::getLayerTwoVertices() const
 {
     return _layerTwoVerticies;
 }
+const sf::VertexArray& TileMap::getLayerThreeVertices() const
+{
+    return _layerThreeVerticies;
+}
+const sf::VertexArray& TileMap::getLayerSixVertices() const
+{
+    return _layerSixVerticies;
+}
+const sf::VertexArray& TileMap::getCanopyVertices() const
+{
+    return _canopyVerticies;
+}
+
 const sf::Texture& TileMap::getTileSet() const
 {
     return _tileset.getTexture();
@@ -210,6 +292,11 @@ const sf::Transform& TileMap::getTransform() const
 {
     return sf::Transformable::getTransform();
 }
+
+
+/************************
+  Tile Animation Handler
+ ************************/
 
 AutoAnimation TileMap::Tile::getTileAnimation(int ID)
 {
@@ -224,7 +311,10 @@ AutoAnimation TileMap::Tile::getTileAnimation(int ID)
         case  6:
         case  7:
         case  8:
-            return AutoAnimation(0,8,16,16,sf::seconds(.2f));
+            return AutoAnimation(0,8,16,16,sf::seconds(.4f));
+            break;
+        case 13:
+            return AutoAnimation(0,5,16,16,sf::seconds(1.0f));
             break;
         //case  :
           //  return AutoAnimation(0,3,16,16,sf::seconds(1.0f));
--- TileMap.h
+++ TileMap.h
@@ -1,6 +1,6 @@
 //Josh Seelye
-#ifndef TileMap_hpp
-#define TileMap_hpp
+#ifndef TileMap_h
+#define TileMap_h
 
 #include <stdio.h>
 #include <vector>
@@ -13,54 +13,111 @@ class TileMap : public sf::Transformable
     class Tile
     {
     private:
+        //handles animation for tiles
         AutoAnimation _animate;
+        
+        //id that correlates to the position in the tilemap
         int _tileID;
+        
+        //current animation step
         int _step;
+        
+        //position of tile on the map
         sf::Vector2f _position;
         
+        //function gets the correct tile animation for each tile
         AutoAnimation getTileAnimation(int ID);
     
     public:
         Tile();
+        
+        //constructs a tile with its correct id starts animation
+        //starts the animation on step one if you want to start on another step call setAnimStep after initialization
+        //sets the position on map
         Tile(int ID, const sf::Vector2f& position);
         
+        //updates the animation of the tile if the current delta time is greater
+        //than the set animation time
+        //if the tile only has one step the function exits before accessing the vertexarray
         void updateAnimStep(sf::Time);
         
+        //sets animation step to a desired animation step regardless of time
+        //goes from [1 _animate.getTotalAnimSteps()]
+        void setAnimStep(unsigned int step);
+        
+        //returns the tile Id for the current tile
         int  getID();
+        
+        //changes a tiles id
         void setID(int ID);
+        
+        //returns the current animation step
         int getAnimStep();
+        
+        //returns the position of the tile
         const sf::Vector2f& getPosition() const;
 
     };
     
 private:
+    //layer 1 with the background being considered layer 0
+    //layer 4 and 5 are sprite sheets
+    //these layers are loaded from top to bottom meaning that canopy will be displayed above ground
     Tile** _ground;
-    Tile** _layerOne;
     Tile** _layerTwo;
+    Tile** _layerThree;
+    Tile** _layerSix;
+    Tile** _canopy;
     TileSet _tileset;
+    
+    //width and height of the current tilemap
     int _width;
     int _height;
+    
+    //containes all of the vertex positions for the textures
 	sf::VertexArray _groundVerticies;
-    sf::VertexArray _layerOneVerticies;
     sf::VertexArray _layerTwoVerticies;
+    sf::VertexArray _layerThreeVerticies;
+    sf::VertexArray _layerSixVerticies;
+    sf::VertexArray _canopyVerticies;
+    
+    //each map will have a identifier and an array that contains all maps they are connected to
+    //this allows us to know what maps should be rendered at any given time
+    int  _mapIdentifier;
+    int* _mapLinks;
+    
+    //background music for each map
+    //this cannot be changed after initialization
     std::string _music;
     
 public:
+    //basic constructor
     TileMap(const sf::Vector2f& scale, std::string music);
     
-    bool build(int* ground, int* layerOne, int* layerTwo, int width, int height, std::string tilesetFileName);
+    //builds the map and returns true on a successful build
+    //send maps with a -1 value for invalid textures
+    //if a map has a -2 value in the 0 position the map will be thrown out
+    bool build(int* ground, int* layerTwo, int* layerThree, int* layerSix, int* canopy, int width, int height, std::string tilesetFileName);
+    
+    //updates all animated tiles in the map
     void updateMap(sf::Time);
     
-    const sf::VertexArray& getGroundVertices()   const;
-    const sf::VertexArray& getLayerOneVertices() const;
-    const sf::VertexArray& getLayerTwoVertices() const;
-    const sf::Texture&     getTileSet()          const;
-    const sf::Transform&   getTransform()        const;
+    //returns the vertexarrays from the corresponding layer
+    const sf::VertexArray& getGroundVertices()     const;
+    const sf::VertexArray& getLayerTwoVertices()   const;
+    const sf::VertexArray& getLayerThreeVertices() const;
+    const sf::VertexArray& getLayerSixVertices()   const;
+    const sf::VertexArray& getCanopyVertices()     const;
+    const sf::Texture&     getTileSet()            const;
+    const sf::Transform&   getTransform()          const;
+    
+    //returns the maps music
     std::string getMusic();
     
     virtual ~TileMap();
 
 private:
+    //fills vertex arrays with tiles that correlate to the from the build phase
 	void vertexFill(int y, int x);
     
 };
