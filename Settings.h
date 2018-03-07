3690ae3ff95a4e1a4a9bb9feb832e422f53cc7a9
Joshua Seelye <jseelye@mail.bradley.edu>
2018-03-06 20:33:06 -0600

    mm

--- Settings.h
+++ Settings.h
@@ -1,27 +1,53 @@
+// Klayton Kowalski
+
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
 #include <SFML/Window.hpp>
 
+// Holds all of the application's meta settings.
+// Whenever a change is about to be made to render window or to some setting, the values
+// in this class should be updated BEFORE any other class. Then, the value that was updated
+// should be PULLED FROM this class and updated in other classes.
+// It boils down to this: When it comes to settings, no other class should ever be ahead of
+// this class. When other classes need settings information, always pull from this class.
 class Settings {
 
 public:
 
+	// Constructor takes an initial video mode to specify in which resolution the render
+	// window should boot.
 	Settings(const sf::VideoMode& initMode = sf::VideoMode(800, 600));
 	~Settings();
 
-	// Mutators
+	// Finds the most efficient fullscreen resolution for the user's particular hardware
+	// then sets the proper settings for fullscreen mode setup.
 	void toggleFullscreen();
+
+	// Sets the intended video mode for render window.
 	void setCurrentVideoMode(const sf::VideoMode& mode);
+
+	// Sets the intended music volume. [0.0f, 100.0f]
 	void setMusicVolume(float volume);
+	
+	// Sets the inteded sound effects volume. [0.0f, 100.0f]
 	void setEffectsVolume(float volume);
 
-	// Accessors
+	// Retrieves the current video mode.
 	const sf::VideoMode& getCurrVideoMode() const;
+
+	// Retrieves all of the possible fullscreen video modes for the user's particular hardware.
+	// The very first video mode of the return vector is the most compatible, and should be used.
 	const std::vector<sf::VideoMode>& getFullscreenVideoModes() const;
+
+	// Are we currently in fullscreen mode?
 	bool isFullscreen() const;
-	const float getMusicVolume() const;
-	const float getEffectsVolume() const;
+
+	// Retrieves the music volume.
+	float getMusicVolume() const;
+
+	// Retrieves the sound effects volume.
+	float getEffectsVolume() const;
 
 private:
 
