fbb4cb994e7437d6b7fd043b3aed58548374d3c2
Joshua Seelye <jseelye@mail.bradley.edu>
2018-03-12 16:58:26 -0500

    reverted tilehandler push from earlier today

--- TileSoundHandler.cpp
+++ TileSoundHandler.cpp
@@ -5,58 +5,56 @@ TileSoundHandler::TileSoundHandler()
 {
 }
 TileSoundHandler::~TileSoundHandler(){}
-bool TileSoundHandler::attachTileSounds(sf::Time deltaTime, sf::FloatRect cameraView, const sf::Vector2f& pposition, const TileMap& tilemap)
+bool TileSoundHandler::attachTileSounds(sf::Time deltaTime, sf::FloatRect cameraView, const sf::Vector2f& pposition, const TileMap& tilemap )
 {
-	for (int i = 0; i < tilemap.getHeight(); i++)
-	{
-		for (int j = 0; j < tilemap.getWidth(); j++)
-		{
-			if (cameraView.contains(tilemap.getGroundTiles()[i][j].getCenterPosition().x * 16.0f, tilemap.getGroundTiles()[i][j].getCenterPosition().y * 16.0f))
-			{
-				if (tilemap.getGroundTiles()[i][j].getSoundFilename().length())
-				{
-					if (findSound(tilemap.getGroundTiles()[i][j].getSoundFilename()) > -1)
-					{
-
-						float radius = sqrt(pow(abs(pposition.x - tilemap.getGroundTiles()[i][j].getCenterPosition().x), 2) + pow(abs(pposition.y - tilemap.getGroundTiles()[i][j].getCenterPosition().y), 2));
-						if (radius < _sounds[findSound(tilemap.getGroundTiles()[i][j].getSoundFilename())].getRadiusFromPlayer() && _soundsPosition[findSound(tilemap.getGroundTiles()[i][j].getSoundFilename())] != tilemap.getGroundTiles()[i][j].getCenterPosition())
-						{
-							_sounds[findSound(tilemap.getGroundTiles()[i][j].getSoundFilename())].setRadiusFromPlayer(radius);
-							_soundsPosition[findSound(tilemap.getGroundTiles()[i][j].getSoundFilename())].x = tilemap.getGroundTiles()[i][j].getCenterPosition().x;
-							_soundsPosition[findSound(tilemap.getGroundTiles()[i][j].getSoundFilename())].y = tilemap.getGroundTiles()[i][j].getCenterPosition().y;
-						}
-						else if (radius != _sounds[findSound(tilemap.getGroundTiles()[i][j].getSoundFilename())].getRadiusFromPlayer() && _soundsPosition[findSound(tilemap.getGroundTiles()[i][j].getSoundFilename())] == tilemap.getGroundTiles()[i][j].getCenterPosition())
-						{
-							_sounds[findSound(tilemap.getGroundTiles()[i][j].getSoundFilename())].setRadiusFromPlayer(radius);
-						}
-					}
-					else
-					{
-						int count = 0;
-						while (_sounds[count].isPlaying()) count++;
-						float radius = sqrt(pow(abs(pposition.x - tilemap.getGroundTiles()[i][j].getCenterPosition().x), 2) + pow(abs(pposition.y - tilemap.getGroundTiles()[i][j].getCenterPosition().y), 2));
-						_tileProperties[count] = tilemap.getGroundTiles()[i][j].getProperties();
-						_sounds[count].setRadiusFromPlayer(radius);
-						_soundsPosition[count].x = tilemap.getGroundTiles()[i][j].getCenterPosition().x;
-						_soundsPosition[count].y = tilemap.getGroundTiles()[i][j].getCenterPosition().y;
-						_sounds[count].setBuffer(tilemap.getGroundTiles()[i][j].getSoundFilename());
-					}
-				}
-				// }
-				if (cameraView.contains(tilemap.getLayerTwoTiles()[i][j].getCenterPosition().x, tilemap.getLayerTwoTiles()[i][j].getCenterPosition().y))
-				{
-
-				}
-				if (cameraView.contains(tilemap.getLayerThreeTiles()[i][j].getCenterPosition().x, tilemap.getLayerThreeTiles()[i][j].getCenterPosition().y))
-				{
-
-				}
-			}
-		}
-		playSounds(deltaTime, cameraView);
-		setLastRadius();
-		return true;
-	}
+    for(int i = 0; i < tilemap.getHeight(); i++)
+    {
+        for(int j = 0; j < tilemap.getWidth(); j++)
+        {
+            //if(cameraView.contains(tilemap.getGroundTiles()[i][j].getCenterPosition().x*7, tilemap.getGroundTiles()[i][j].getCenterPosition().y*7))
+            //{
+            if(tilemap.getGroundTiles()[i][j].getSoundFilename().length())
+            {
+                if(findSound(tilemap.getGroundTiles()[i][j].getSoundFilename()) > -1)
+                {
+                    
+                    float radius = sqrt(pow(abs(pposition.x-tilemap.getGroundTiles()[i][j].getCenterPosition().x),2)+pow(abs(pposition.y-tilemap.getGroundTiles()[i][j].getCenterPosition().y),2));
+                    if(radius < _sounds[findSound(tilemap.getGroundTiles()[i][j].getSoundFilename())].getRadiusFromPlayer() && _soundsPosition[findSound(tilemap.getGroundTiles()[i][j].getSoundFilename())] != tilemap.getGroundTiles()[i][j].getCenterPosition())
+                    {
+                        _sounds[findSound(tilemap.getGroundTiles()[i][j].getSoundFilename())].setRadiusFromPlayer(radius);
+                        _soundsPosition[findSound(tilemap.getGroundTiles()[i][j].getSoundFilename())].x = tilemap.getGroundTiles()[i][j].getCenterPosition().x;
+                        _soundsPosition[findSound(tilemap.getGroundTiles()[i][j].getSoundFilename())].y = tilemap.getGroundTiles()[i][j].getCenterPosition().y;
+                    }
+                    else if(radius != _sounds[findSound(tilemap.getGroundTiles()[i][j].getSoundFilename())].getRadiusFromPlayer()&& _soundsPosition[findSound(tilemap.getGroundTiles()[i][j].getSoundFilename())] == tilemap.getGroundTiles()[i][j].getCenterPosition())
+                    {
+                        _sounds[findSound(tilemap.getGroundTiles()[i][j].getSoundFilename())].setRadiusFromPlayer(radius);
+                    }
+                }
+                else
+                {
+                    int count = 0;
+                    while(_sounds[count].isPlaying()) count++;
+                    float radius = sqrt(pow(abs(pposition.x-tilemap.getGroundTiles()[i][j].getCenterPosition().x),2)+pow(abs(pposition.y-tilemap.getGroundTiles()[i][j].getCenterPosition().y),2));
+                    _tileProperties[count] = tilemap.getGroundTiles()[i][j].getProperties();
+                    _sounds[count].setRadiusFromPlayer(radius);
+                    _soundsPosition[count].x = tilemap.getGroundTiles()[i][j].getCenterPosition().x;
+                    _soundsPosition[count].y = tilemap.getGroundTiles()[i][j].getCenterPosition().y;
+                    _sounds[count].setBuffer(tilemap.getGroundTiles()[i][j].getSoundFilename());
+                }
+            }
+            // }
+            if(cameraView.contains(tilemap.getLayerTwoTiles()[i][j].getCenterPosition().x, tilemap.getLayerTwoTiles()[i][j].getCenterPosition().y))
+            {
+                
+            }
+            if(cameraView.contains(tilemap.getLayerThreeTiles()[i][j].getCenterPosition().x, tilemap.getLayerThreeTiles()[i][j].getCenterPosition().y))
+            {
+                
+            }
+        }
+    }
+    playSounds(deltaTime, cameraView);
+    setLastRadius();
 }
 
 void TileSoundHandler::playSounds(sf::Time deltaTime, sf::FloatRect cameraView)
@@ -67,8 +65,8 @@ void TileSoundHandler::playSounds(sf::Time deltaTime, sf::FloatRect cameraView)
         {
             //if(cameraView.contains(_soundsPosition[i].x * 7, _soundsPosition[i].y * 7))
             //{
-                _sounds[i].setVolume(*_volume - _sounds[i].getRadiusFromPlayer());
-                _sounds[i].playSound(deltaTime);
+            _sounds[i].setVolume(*_volume - _sounds[i].getRadiusFromPlayer());
+            _sounds[i].playSound(deltaTime);
             //}
         }
     }
